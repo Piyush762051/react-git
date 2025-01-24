7fd6b8ca16d8c70a1f81f94282c2de6e3c404bc9
Piyush762051 <khandarepiyush2@gmail.com>
2025-01-24 16:15:48 +0530

    Header File add in Template Folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Template/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header/>
       <div>
-        <h1>Piyush</h1>
+        <h1>Project:- React-Git</h1>
       </div>
     </>
   )
---
+++ src/Template/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() 
+{
+  return (
+    <div>
+        <h1>How to Push the React Project on gitHub</h1>
+    </div>
+  )
+}
+
+export default Header
